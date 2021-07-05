c91412c1a77662ecbe641c03e28d24b7134431a5
Kyle Escobar <82987787+kyleescobar@users.noreply.github.com>
2021-07-05 00:30:18 -0400

    Rebase project to initial

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,22 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+/*
+ * Copyright (C) Spectral Powered <Kyle Escobar>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 plugins {
-    idea
     kotlin("jvm") version "1.5.20"
-    id("org.cadixdev.licenser") version "0.5.0"
 }
 
 tasks.withType<Wrapper> {
@@ -23,13 +36,5 @@ allprojects {
 
     dependencies {
         implementation(kotlin("stdlib"))
-    }
-
-    tasks.withType<KotlinCompile> {
-        kotlinOptions {
-            jvmTarget = JavaVersion.VERSION_1_8.toString()
-            sourceCompatibility = JavaVersion.VERSION_1_8.toString()
-            targetCompatibility = JavaVersion.VERSION_1_8.toString()
-        }
     }
 }
